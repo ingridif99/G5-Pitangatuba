f6098ed34c94db2b3d3c3f6c7317141a91f8d359
talimenger <talitamenger.r@gmail.com>
2021-11-03 16:35:12 -0300

    add script funcao para segmentar rodovia

---
+++ Scripts/SegmentSpatialLines_function.R
@@ -0,0 +1,87 @@
+
+CreateSegment <- function(coords, from, to) {
+  distance <- 0
+  coordsOut <- c()
+  biggerThanFrom <- F
+  for (i in 1:(nrow(coords) - 1)) {
+    d <- sqrt((coords[i, 1] - coords[i + 1, 1])^2 + (coords[i, 2] - coords[i + 
+                                                                             1, 2])^2)
+    distance <- distance + d
+    if (!biggerThanFrom && (distance > from)) {
+      w <- 1 - (distance - from)/d
+      x <- coords[i, 1] + w * (coords[i + 1, 1] - coords[i, 1])
+      y <- coords[i, 2] + w * (coords[i + 1, 2] - coords[i, 2])
+      coordsOut <- rbind(coordsOut, c(x, y))
+      biggerThanFrom <- T
+    }
+    if (biggerThanFrom) {
+      if (distance > to) {
+        w <- 1 - (distance - to)/d
+        x <- coords[i, 1] + w * (coords[i + 1, 1] - coords[i, 1])
+        y <- coords[i, 2] + w * (coords[i + 1, 2] - coords[i, 2])
+        coordsOut <- rbind(coordsOut, c(x, y))
+        break
+      }
+      coordsOut <- rbind(coordsOut, c(coords[i + 1, 1], coords[i + 1, 
+                                                               2]))
+    }
+  }
+  return(coordsOut)
+}
+
+CreateSegments <- function(coords, length = 0, n.parts = 0) {
+  stopifnot((length > 0 || n.parts > 0))
+  # calculate total length line
+  total_length <- 0
+  for (i in 1:(nrow(coords) - 1)) {
+    d <- sqrt((coords[i, 1] - coords[i + 1, 1])^2 + (coords[i, 2] - coords[i + 
+                                                                             1, 2])^2)
+    total_length <- total_length + d
+  }
+  
+  # calculate stationing of segments
+  if (length > 0) {
+    stationing <- c(seq(from = 0, to = total_length, by = length), total_length)
+  } else {
+    stationing <- c(seq(from = 0, to = total_length, length.out = n.parts), 
+                    total_length)
+  }
+  
+  # calculate segments and store the in list
+  newlines <- list()
+  for (i in 1:(length(stationing) - 1)) {
+    newlines[[i]] <- CreateSegment(coords, stationing[i], stationing[i + 1])
+  }
+  return(newlines)
+}
+
+MergeLast <- function(lst) {
+  l <- length(lst)
+  lst[[l - 1]] <- rbind(lst[[l - 1]], lst[[l]])
+  lst <- lst[1:(l - 1)]
+  return(lst)
+}
+
+SegmentSpatialLines <- function(sl, length = 0, n.parts = 0, merge.last = FALSE) {
+  stopifnot((length > 0 || n.parts > 0))
+  id <- 0
+  newlines <- list()
+  sl <- as(sl, "SpatialLines")
+  for (lines in sl@lines) {
+    for (line in lines@Lines) {
+      crds <- line@coords
+      # create segments
+      segments <- CreateSegments(coords = crds, length, n.parts)
+      if (merge.last && length(segments) > 1) {
+        # in case there is only one segment, merging would result into error
+        segments <- MergeLast(segments)
+      }
+      # transform segments to lineslist for SpatialLines object
+      for (segment in segments) {
+        newlines <- c(newlines, Lines(list(Line(unlist(segment))), ID = as.character(id)))
+        id <- id + 1
+      }
+    }
+  }
+  return(SpatialLines(newlines))
+}
