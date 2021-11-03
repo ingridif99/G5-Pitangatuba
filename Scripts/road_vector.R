da3836ce0bdad93a99bf7dcbd9448646c104f149
talimenger <talitamenger.r@gmail.com>
2021-11-03 16:34:42 -0300

    modificamos importacao e segmentacao da rodovia no script

--- Scripts/road_vector.R
+++ Scripts/road_vector.R
@@ -11,38 +11,37 @@ library(readxl)
 library(sf)
 library(rgeos)
 
+#OBS: Etapas comentadas abaixo foram a primeira tentativa para importar e segmentar a rodovia que foi substituída pelo códico seguinte (Guardamos para possível consulta)
+
 ###### entrando com o shape das rodovias federais
-road <- st_read(here::here("Variaveis", "rodovias", "Rodovias.shp"), quiet = TRUE)
+#road <- st_read(here::here("Variaveis", "rodovias", "Rodovias.shp"), quiet = TRUE)
+#road050 <- road %>% #filtrando apenas a rodovia de interesse BR050
+#  filter(vl_br == "050")
+# plot(road050$geometry) #visualizando a nossa rodovia de interesse
+# road050_x<- road050 %>%  #retirei o anel em Uberlandia para termos a rodovia em linha mesmo
+#filter(nm_tipo_tr == "Eixo Principal" |
+#         nm_tipo_tr == "Acesso" |
+#         nm_tipo_tr == "Variante")
+#plot(road050_x$geometry)
+#br050_union <- st_union(road050_x) #unindo todos os trechos da rodovia em uma linha - all geometries are unioned together
 
-#Simple feature collection with 7360 features and 30 fields
-#Geometry type: LINESTRING
-#Dimension:     XY
-#Bounding box:  xmin: -73.70507 ymin: -33.69308 xmax: -32.40022 ymax: 4.482599
-#Geodetic CRS:  SIRGAS 2000
-                
-plot(road$geometry) #visualizando as rodovias federais no brasil
 
-road050 <- road %>% #filtrando apenas a rodovia de interesse BR050
-  filter(vl_br == "050")
+##### entrando com o shape da rodovia do google mymaps
+br050 <- st_read(here::here("Variaveis", "rodovias", "br050_mymaps.shp"), quiet = TRUE)
 
-plot(road050$geometry) #visualizando a nossa rodovia de interesse
+plot(br050$geometry) #visualizando a rodovia 050
 
-road050_x<- road050 %>%  #retirei o anel em Uberlandia para termos a rodovia em linha mesmo
-  filter(nm_tipo_tr == "Eixo Principal" |
-              nm_tipo_tr == "Acesso" |
-              nm_tipo_tr == "Variante")
-plot(road050_x$geometry)
+br050_ext <- sum(st_length(br050)) #extensao em metros da rodovia
 
-br050_ext <- sum(st_length(road050_x)) #extensao em metros da rodovia
+br050_geometry <- br050$geometry #selecionou apenas a coluna das coordenadas do df para ser um objeto class spatial lines
 
-br050_union <- st_union(road050_x) #unindo todos os trechos da rodovia em uma linha - all geometries are unioned together
-#road050_union <- st_combine(road050_x) # Combine several feature geometries into one, without unioning or resolving internal boundaries
+br050_sp <- as_Spatial(br050_geometry) #transformando em objeto espacial SP
 
-plot(br050_union)
+source("SegmentSpatialLines_function.R") #puxando função para dividir br em segmentos
 
-br050 <- st_as_sf(br050_union) #Transformei o objeto multilinestring em espacial.
-
-#as_Spatial(br050) #objeto espacial SP
+#segmentos de 5km
+segments_br050 <- SegmentSpatialLines(br050_sp, length = 0.045) # o comprimento na função é dado em metros, nossas coords estão em graus (1grau = 111km)
+plot(segments_br050, col= rainbow(8), lwd=3) #visualizando a estrada segmentada
 
 ##### etapa extra para auxiliar no recorte do raster posteriormente
 road_buffer <- st_buffer(x = road050$geometry, dist = 15000) #estabelecendo um buffer ao entorno da rodovia para depois recortarmos o raster
