4c118aed00b6d7b6f6eb0694d14cc645f0c708a0
ingridif99 <ingridicfranceschi@hotmail.com>
2021-11-04 10:05:28 -0300

    novo raster de vegetação ao entorno do novo trecho da rodovia.

---
+++ Scripts/raster_crop_mask.R
@@ -0,0 +1,21 @@
+#pacotes necessarios para rodar esse script
+library(here)
+library(tidyverse)
+library(sf)
+library(raster)
+
+
+#### visualizando o raster da paisagem entorno da rodovia
+raster <- raster::raster(here::here("Variaveis", "vegetacao", "mapbiomas_2013.tif"))
+plot(raster)
+
+##### entrando com o shape da rodovia do google mymaps
+br050_y <- st_read(here::here("Variaveis", "rodovias", "br050_buffer.shp"), quiet = TRUE)
+plot(br050_y) #visualizando o shapefile
+
+#ajustando e cortando o raster com base no shapefile - novo trecho da rodovia
+road_crop_mask <- raster %>% 
+  crop(br050_y) %>% 
+  mask(br050_y)
+
+plot(road_crop_mask) #visualizando
