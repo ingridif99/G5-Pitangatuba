ecf52cf011d06604fe1bf2767642d0a215aabbf1
ingridif99 <ingridicfranceschi@hotmail.com>
2021-11-05 11:11:29 -0300

    add graficos.

---
+++ Scripts/glm.R
@@ -0,0 +1,46 @@
+#' ---
+#' title: modelos glm para ver relação das variáveis com os atropelamentos na br050
+#' author:grupo pitangatuba
+#' date: 04/11/2021
+#' ---
+library(MASS)
+library(performance)
+library(see)
+
+#modelo glm com distribuição binomial negativa (vatiável resposta contagem)
+#testar se a distancia dos segmentos da rodovia para corpos dagua e floresta 
+#influencia nos atropelamentos de mamíferos de medio e grande porte (br 050)
+modelo <- glm.nb(seg_fatal ~ min.dist_agua*min.dist_flo, data = dados_glm)
+summary(modelo)
+plot(modelo)
+
+hist(min.dist_agua)
+hist(min.dist_flo)
+hist(seg_fatal,
+     main = "Fatalidades de mamíferos de médio e grande porte na BR-050",
+     xlab = "Fatalidades",
+     ylab = "Frequência")
+plot(dados_glm$seg_fatal ~ min.dist_agua,
+     ylab = "Fatalidades",
+     xlab = "Distância mínima para corpos d'água")
+plot(dados_glm$seg_fatal ~ min.dist_flo,
+     ylab = "Fatalidades",
+     xlab = "Distância mínima para fragmentos florestais")
+
+#The overdispersion statistic can be calculated
+ods <- modelo$deviance / modelo$df.residual; ods #underdispersion
+
+#usando os pacotes performance e see para checar a sobredispersao e residuos
+#check_overdispersion(modelo)
+check_model(modelo)
+
+# validacao do modelo pelos residuos
+Fitted <- fitted(modelo)
+Resid <- resid(modelo, type = "pearson")
+par(mfrow = c(1,1), mar = c(5,5,2,2))
+plot(x = Fitted, y = Resid,
+     xlab = "Fitted values",
+     ylab = "Pearson Residuals")
+abline(h = 0, lty = 2)
+
+
